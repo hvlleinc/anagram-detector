4c3fc4a98420dfb3421d1d7d274863563bf51d20
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2021-01-15 00:08:33 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+ attr_accessor :anagram
+
+ def initialize(anagram)
+    @anagram = anagram
+ end
+
+ def match(array)
+ 
+    array.select do |x| 
+        x.split("").sort == @anagram.split("").sort
+    end
+ end
+
+end
